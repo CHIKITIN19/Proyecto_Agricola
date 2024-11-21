b32d2d43bed930f50305767fcb6e1eb1fb00afbf
CHIKITIN19 <141179159+CHIKITIN19@users.noreply.github.com>
2024-11-20 22:52:16 -0600

    Se inicializa el repo

---
+++ src/Model/Trabajadores/Trabajadores.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.Trabajadores;
+
+/**
+ *
+ * @author 9567
+ */
+public class Trabajadores {
+    
+}
